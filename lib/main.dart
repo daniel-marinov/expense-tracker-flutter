caa9868638d8da349f70819b4fc07610dd6a6303
danielmarinoff <daniellmarinoff@gmail.com>
2023-09-30 01:44:50 +0300

    first-commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:exp_tracker/widgets/expenses.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Expenses(),
+    ),
+  );
+}
